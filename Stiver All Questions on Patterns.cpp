984acf2d5c13652334d560b234365cc4ce1a1c92
Somnathjha007 <107018077+Somnathjha007@users.noreply.github.com>
2024-02-23 11:33:16 +0530

    Add files via upload

---
+++ Stiver All Questions on Patterns.cpp
@@ -0,0 +1,342 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void pattern1(int row , int col){
+//     for(int i =0;i<row;i=i+1){
+//         for(int j =0;j<col;j=j+1){
+//             cout<<"*";
+//         }
+//         cout<<"\n";
+//      }
+// }
+
+// // void pattern2(int n ){
+// //     for(int i =0;i<n;i++){
+// // 		for(int j =0;j<=i;j++){
+// // 			cout<<i+1;
+// // 		}
+// // 		cout<<"\n";
+// // 	}
+// // }
+// // void patternTwo(int n ){
+// // for(int i =0;i<n;i++){
+// // 		for(int j = 1;j<=n-i;j++){
+// // 			cout<<"*"<< " ";
+// // 		}
+// // 		cout<<"\n";
+// // 	}
+// // }
+
+// // void pattern7(int n ){
+// //     for(int i =0;i<n;i++){
+// // 		for(int j =0;j<n-i-1;j++)
+// //}
+// }//         {
+// //             cout<<" ";
+// //         }
+// //         for(int j =0;j<2*i+1;j++){
+// //             cout<<"*";
+// //         }
+// //         for(int j =0;j<n-i-1;j++)
+// //         {
+// //             cout<<" ";
+// //         }
+// //         cout<<"\n";
+// //  	}
+// //  }
+// // void pattern8(int n ){
+// //     for(int i =0;i<n;i++){
+// // 		for(int j =0;j<n-i-1;j++)
+// //         {
+// //             cout<<" ";
+// //         }
+// //         for(int j =0;j<2*i+1;j++){
+// //             cout<<j+1;
+// //         }
+// //         for(int j =0;j<n-i-1;j++)
+// //         {
+// //             cout<<" ";
+// //         }
+// //         cout<<"\n";
+	
+// void pattern9(int n){
+//     for(int i =0;i<n;i++){
+//        for(int j =0;j<i;j++)
+//         {
+//             cout<<" ";
+//         }
+//         for(int j =0;j<2*n-(2*i+1);j++){
+//             cout<<"*";
+//         }
+//         for(int j =0;j<i;j++)
+//         {
+//             cout<<" ";
+//         }
+//         cout<<"\n"; 
+//     }
+// }
+
+// void pattern10(int n){
+//     for(int i =1;i<=2*n-1;i++){
+//         int star = i;
+//         if(i>n){
+//             star = 2*n-i;
+//         }
+//         for(int j=1;j<=star;j++){
+//             cout<<"* ";
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// void pattern11(int n){
+//     int start = 0 ; 
+    
+//     for(int i =1;i<=2*n-1;i++){
+//         int bit =i;
+//         if(i%2==0) start = 1;
+//         else start=0;
+//         if(i>n){
+//             bit = 2*n-i;
+//            }
+//         for(int j =1;j<=bit;j++){
+//            cout<<start;
+//            start = 1-start;
+           
+
+
+//         }
+//         cout<<"\n";
+        
+//     }
+// }
+
+void pattern12(int n){
+    
+    for(int i =1;i<=n;i++){
+        // int count =0;
+        // for(int j=0;j<i;j++){
+        //     cout<<count+1;
+        //     count++;
+            
+            
+        // }
+        // for(int j =0;j<2*n-2*i;j++){
+        //     cout<<" ";
+        // }
+        // for(int j=0;j<i;j++){
+        //     count--;
+        //     cout<<count+1;
+            
+            
+        // }
+
+        //numbers
+        for(int j=1;j<=i;j++){cout<<j;}
+
+        //space
+        for(int j =0;j<2*n-2*i;j++){cout<<" ";}
+
+        //numbers
+        for(int j=i;j>=1;j--){cout<<j;}
+        
+        cout<<"\n";
+
+    }
+}
+// void pattern13(int n){
+//     int count =1;
+//     for(int i=0;i<=n;i++){
+//         for(int j =0;j<i;j++){
+//             cout<<count<<" ";
+//             count++;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// void pattern14(int n){
+    
+//     // for(int i =0;i<n;i++){
+//     //     char c = 'A';
+//     //     for(int j=0;j<=i;j++){
+//     //         cout<<c++;
+//     //     }
+//     //     cout<<"\n";
+//     // }
+//     for(int i =0;i<n;i++){
+//         for(char ch = 'A';ch<='A'+i;ch++){
+//             cout<<ch;
+//         }
+//         cout<<"\n";
+//     }
+// }
+// void pattern15(int n){
+//     for(int i =0;i<=n;i++){
+//         for(char ch = 'A';ch<'A'+n-i;ch++){
+//             cout<<ch;
+//         }
+//         cout<<endl;
+//     }
+// }
+// void pattern16(int n){
+//     char ch ='A';
+//     for(int i =0;i<n;i++){
+//         for(int j =0;j<=i;j++){
+//             cout<<ch;
+//         }
+//         cout<<endl;
+//         ch++;
+//     }
+// }
+
+// void pattern17(int n){
+//  for(int i =0;i<n;i++){
+//     char ch ='A';
+//     for(int j =1;j<=n-i;j++){
+//         cout<<" ";
+//     }
+//     for(int j =0;j<2*i+1;j++){
+//         if(j>=i){
+//             cout<<ch--;
+//         }
+//         else {cout<<ch++;}
+//     }
+//     for(int j =1;j<=n-i;j++){
+//         cout<<" ";
+//     }
+//     cout<<endl;
+//  }
+// }
+// void pattern19(int n){
+//     // for(int i =1;i<=n;i++){
+//     //     char ch ='A'+n-i;
+//     //     for(int j = 1;j<=i;j++){
+//     //         cout<<ch++;
+//     //     }
+//     //     cout<<endl;
+//     // }
+
+// }
+// void pattern20(int n){
+//     int count = 0;
+//     for(int i =0;i<=2*n+1;i++){
+        
+//         if(i>n){
+            
+//             for(int j =0;j<i-n;j++){
+//                 cout<<"*";
+//             }
+//             for(int j =0;j<2*n-2*count;j++){
+//                 cout<<" ";
+//             }
+//             for(int j =0;j<i-n;j++){
+//                 cout<<"*";
+//             }
+//             cout<<endl;
+//             count++;
+            
+//         }
+
+//         else {
+//         for(int j =0;j<n+1-i;j++){
+//             cout<<"*";
+//         }
+//         for(int j =0;j<2*i;j++){
+//             cout<<" ";
+//         }
+//         for(int j =0;j<n+1-i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     }
+// }
+
+// void pattern21(int n){
+//     for(int i =0;i<=2*n+1;i++){
+//         if(i>n){
+//             for(int j=0;j<=2*n-i;j++){
+//                 cout<<"*";
+//             }
+//             for(int j =0;j<=2*i-2*n-1;j++){
+//                 cout<<" ";
+//             }
+//             for(int j=0;j<=2*n-i;j++){
+//                 cout<<"*";
+//             }
+//             cout<<endl;
+            
+//         }
+//        else{
+//             for(int j =0;j<=i;j++){
+//                 cout<<"*";
+//             }
+//             for(int j =1;j<2*n-2*i+1;j++){
+//                 cout<<" ";
+//             }
+//             for(int j =0;j<=i;j++){
+//                 cout<<"*";
+//             }
+//             cout<<endl;
+            
+//         }
+//     }
+// }
+// void pattern22(int n){
+//     for(int i =0;i<n;i++){
+//         cout<<"*";
+//     }
+//     cout<<endl;
+//     for(int i =0;i<n/2;i++){
+//         for(int j =0;j<1;j++){
+//             cout<<"*";
+//         }
+//         for(int j =0;j<n-2;j++){
+//             cout<<" ";
+//         }  
+//         for(int j =0;j<1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     for(int i =0;i<n;i++){
+//         cout<<"*";
+//     }
+//     cout<<endl;
+// }
+
+// void pattern22(int n){
+//     for(int i =0;i<n;i++){
+//         for(int j =0;j<n;j++){
+//             if(i==0||j==0||i==n-1||j==n-1){
+//                 cout<<"*";
+//             }
+//             else { cout<<" ";}
+//         }
+//         cout<<endl;
+//     }
+// }
+void pattern23(int n ){
+    
+    for(int i =0;i<2*n-1;i++){
+        
+        for(int j=0;j<2*n-1;j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int down = (2*n-2)-i;
+            cout<<(n- min(min(top,down),min(left,right)));
+
+        }
+        cout<<endl;
+        
+    }
+}
+
+int main(){
+    
+     pattern23(4);
+     
+
+}
